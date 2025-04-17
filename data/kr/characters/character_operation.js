486304b5c1ebb9e6a62ffe420a7d2838aeca79f4
absolroot <superphil722@gmail.com>
2025-04-17 16:30:11 +0900

    v1.6.5 6

--- data/kr/characters/character_operation.js
+++ data/kr/characters/character_operation.js
@@ -4,8 +4,12 @@ const operationData = {
         note: []
     },
     "이케나미": {
-        basic: [],
-        note: []
+        basic: [
+            { label: "의식 0", value: "1 › 3 › 1 › 3 › 1 › 3" },
+        ],
+        note: [
+            "· 패시브 및 해명의 힘에 의해 전투 시 공격력 상승률이 높으므로 계시 세팅 맞춰 조정",
+        ]
     },
     "마나카": {
         basic: [

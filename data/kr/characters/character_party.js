acbc566860adc3f197784d32165c3a34f0117bfa
absolroot <superphil722@gmail.com>
2025-07-25 11:37:51 +0900

    v2.1.4

---
+++ data/kr/characters/character_party.js
@@ -0,0 +1,40 @@
+const recommendParty = {
+    "유스케": {
+        "persona": [
+            {
+                type: "BUFF",
+                list: ["디오니소스", "서큐버스"],
+                comment: "크리티컬 확률과 효과를 높이기 위한 페르소나",
+                comment_en: "Increase critical rate and effect",
+                comment_jp: "クリティカル確率と効果を高めるためのペルソナ"
+            },
+            {
+                type: "DEF",
+                list: ["산달폰", "미트라스", "아프사라스", "킹프로스트"],
+                comment: "실드 및 방어력을 올려주는 페르소나",
+                comment_en: "Increase shield and defense",
+                comment_jp: "盾と防御力を上げるペルソナ"
+            }
+        ],
+        "party": [
+            {
+                members: ["유스케", "미나미", "유키미", "미유"],
+                comment: "미나미(MARIAN) 의식(Awareness)1의 관통 20%를 활용하기 위한 조합",
+                comment_en: "Utilize MARIAN's Awareness 1's 20% penetration",
+                comment_jp: "MARIANの意識1の20%貫通を活用するためのパーティ"
+            },
+            {
+                members: ["유스케", "마사키", "유키미", "유우미"],
+                comment: "실드를 빠르게 올려 유스케의 패시브를 활성화",
+                comment_en: "Increase shield to activate FOX's passive",
+                comment_jp: "FOXのパッシブを活用するためのパーティ"
+            },
+            {
+                members: ["유스케", "마사키", "아야카", "유우미"],
+                comment: "아야카의 스킬3 HIGHLIGHT를 통해 반격 확률을 100%로 올림",
+                comment_en: "Utilize CHORD's skill 3 HIGHLIGHT to raise counter rate to 100%",
+                comment_jp: "CHORDのスキル3 HIGHLIGHTを活用して反撃確率を100%にするパーティ"
+            }
+        ]
+    }
+};

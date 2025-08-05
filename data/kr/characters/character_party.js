a6ea384fe0f48e98911b5848c965805310a42422
absolroot <superphil722@gmail.com>
2025-08-06 08:05:35 +0900

    2.2.3 2

--- data/kr/characters/character_party.js
+++ data/kr/characters/character_party.js
@@ -421,15 +421,15 @@ const recommendParty = {
         "party": [
             {
                 members: ["마코토", "치즈코", "레오", "카요"],
-                comment: "",
-                comment_en: "",
-                comment_jp: ""
+                comment: "VINO를 통해 방어력 감소와 원소 이상을 부여합니다.",
+                comment_en: "Reduce DEF and inflict elemental ailments through VINO.",
+                comment_jp: "VINOによりDEF減少と元素異常を付与する。"
             },
             {
                 members: ["마코토", "치즈코", "레오", "유우미"],
-                comment: "",
-                comment_en: "",
-                comment_jp: ""
+                comment: "핵열 속성을 공통적으로 사용하므로 PHOEBE와의 조합이 유리합니다.",
+                comment_en: "Since both characters use the Nuclear attribute, it is advantageous to combine with PHOEBE.",
+                comment_jp: "核熱属性を共通で使用するため、PHOEBEとの組み合わせが有利です。"
             },
             {
                 members: ["마코토", "치즈코", "아야카", "후타바"],

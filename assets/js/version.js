2719e69006a06a3d99897d24de1bdc93daebe8be
absolroot <superphil722@gmail.com>
2025-12-02 03:12:32 +0900

    fix bug4

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2.9.9';  // 현재 앱 버전
+const APP_VERSION = '3.0.0';  // 현재 앱 버전
 
 class VersionChecker {
     static check() {
---
+++ data/characters/레오/party.js
@@ -0,0 +1,36 @@
+window.recommendParty = window.recommendParty || {};
+window.recommendParty["레오"] = {
+    "weapon": [
+        ""
+    ],
+    "persona": [
+        {
+            "type": "ATK",
+            "list": [
+                "",
+                "",
+                ""
+            ]
+        },
+        {
+            "type": "CRITICAL",
+            "list": [
+                "",
+                ""
+            ]
+        }
+    ],
+    "party": [
+        {
+            "members": [
+                "",
+                "",
+                "",
+                ""
+            ],
+            "comment": "",
+            "comment_en": "",
+            "comment_jp": ""
+        }
+    ]
+};

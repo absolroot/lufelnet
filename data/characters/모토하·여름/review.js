1d3804d1dd6f62651fac7d0aec7998e8c4006bfe
absolroot <superphil722@gmail.com>
2025-12-07 21:52:51 +0900

    BACKUP 12.07

--- data/characters/모토하·여름/review.js
+++ data/characters/모토하·여름/review.js
@@ -2,7 +2,7 @@
     window.characterReview = window.characterReview || {};
     window.characterReview["모토하·여름"] = { name_en: "Seaside Tomoko", name_jp: "朋子・ピーチサマー", codename: "MOKO·Summer",
         review: `
-최초의 **축복 속성 딜러**다.
+괴도단 멤버이자 최초의 **축복 속성 광역 딜러**로 여름 이벤트로 출시됐다.
 
 스킬을 사용할 때 자신의 HP를 소모하며, 특히 **핵심 스킬인 3스킬**은 **모토하가 HP를 회복할 때마다 쿨타임이 줄어드는 효과**가 있다. 덕분에 미나미나 토모코·여름 같은 **힐러와 함께 조합**하는 경우가 많다.  
 
@@ -12,7 +12,7 @@
 
 생명력이 중요한 캐릭터 특성 상 생존성이 매우 좋고, 사이클도 단순한 편이라 **오토로 플레이하기에 적절**하다.`,
 review_en: `
-The first **Bless-type DPS**.
+The first **Bless-type AoE DPS** released as part of the Summer event.
 
 She consumes her own HP to use skills, and her **core Skill 3** can remove its cooldown through stacks Motoha gains by **restoring HP**, which naturally pairs her with healers such as **Minami Miyashita** or **Tomoko Noge·Summer**.
 
@@ -21,7 +21,7 @@ The higher her **HP**, the greater her **DMG Bonus (ATK Mult)** and **ATK** gain
 However, there’s no benefit beyond **16,000 HP**, so after accounting for party buffs, it’s best to stop stacking HP there and invest remaining stats into offensive options.
 `,
 review_jp: `
-初の**祝福属性アタッカー**。
+夏イベントの一環として登場した初の**祝福属性全体攻撃アタッカー**。
 
 自身のHPを消費してスキルを使用し、**核心となるスキル3**は、モトハがHPを回復して獲得するスタックを通じてクールダウンを解除できる。**宮下 美波**や**野毛 朋子・夏**といったヒーラーとの編成が前提となる。
 

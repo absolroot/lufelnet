a6b2e62f7947d48851581eab29800a49cd853c45
absolroot <superphil722@gmail.com>
2026-05-01 10:15:02 +0900

    4.7.8 3

--- data/characters/아이기스/base_stats.js
+++ data/characters/아이기스/base_stats.js
@@ -1,10 +1,10 @@
 window.basicStatsData = window.basicStatsData || {};
 window.basicStatsData["아이기스"] = {
     "a0_lv1": {
-        "HP": 0,
-        "SP": 0,
-        "attack": 0,
-        "defense": 0,
+        "HP": 305,
+        "SP": 100,
+        "attack": 106,
+        "defense": 53,
         "crit_rate": 5,
         "crit_mult": 150,
         "speed": 95

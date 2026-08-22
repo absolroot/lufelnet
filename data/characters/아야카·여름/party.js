54f903e2461c677979b11e35e2ab5b9cd396d503
absolroot <superphil722@gmail.com>
2026-08-22 19:32:53 +0900

    파티

--- data/characters/아야카·여름/party.js
+++ data/characters/아야카·여름/party.js
@@ -44,17 +44,17 @@ window.recommendParty["아야카·여름"] = {
     "friends": [
         {
             "name": "모르가나·댄싱 스타",
-            "comment": "",
-            "comment_en": "",
-            "comment_jp": "",
-            "comment_cn": ""
+            "comment": "적 전체의 총 방어력을 크게 감소시키며, 질풍 대미지 보너스와 더불어 속성 심상 스킬의 시너지 효과를 함께 발동시킬 수 있다.",
+            "comment_en": "Greatly reduces all foes' total DEF while also providing a Wind damage bonus and activating Attribute Mindscape's synergy effect.",
+            "comment_jp": "敵全体の総防御力を大幅に低下させ、疾風属性ダメージボーナスに加えて属性イメジャリーのシナジー効果も発動できる。",
+            "comment_cn": "可大幅降低所有敌人的总防御力，并在提供疾风属性伤害加成的同时触发属性心象的协同效果。"
         },
         {
             "name": "리코·매화",
-            "comment": "",
-            "comment_en": "",
-            "comment_jp": "",
-            "comment_cn": ""
+            "comment": "크리티컬 효과 및 공격력을 제공할 수 있으며, 속성 심상 스킬을 통해 높은 질풍 속성 대미지 증폭을 기대해볼 수 있다.",
+            "comment_en": "Can provide CRIT Mult and ATK, while her Attribute Mindscape skill offers substantial Wind damage amplification.",
+            "comment_jp": "クリティカル倍率と攻撃力を提供でき、属性イメジャリーのスキルによる高い疾風属性ダメージ増幅も期待できる。",
+            "comment_cn": "可提供暴击效果与攻击力，并可通过属性心象技能获得高额疾风属性伤害增幅。"
         }
     ]
 };

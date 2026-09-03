08c46abac295f0a5f05d69959ff3732685c8c910
absolroot <superphil722@gmail.com>
2026-09-03 14:05:25 +0900

    ichigo party

--- data/characters/이치고·여름/party.js
+++ data/characters/이치고·여름/party.js
@@ -16,21 +16,24 @@ window.recommendParty["이치고·여름"] = {
     "friends": [
         {
             "name": "나루미",
-            "comment": "염동 동료라 『환상의 인장』을 활성화하고, 조타수로 지정해 추가 공격을 활용할 수 있다.",
-            "comment_en": "",
-            "comment_jp": ""
+            "comment": "염동 속성 동료로서 『환상의 인장』을 활성화하고, 조타수로 지정해 추가 공격을 활용할 수 있다.",
+            "comment_en": "As a Psy ally, activates [Illusion Seal] and can be designated as the [Helmsman] to make use of the additional attack.",
+            "comment_jp": "念動属性の仲間として『幻の印』を発動し、『操舵手』に指定して追加攻撃を活用できる。",
+            "comment_cn": "作为念动属性队员，可激活『幻之印记』，并指定为『舵手』以利用追加攻击。"
         },
         {
             "name": "하루",
-            "comment": "염동 동료라 『환상의 인장』을 활성화하며, 조타수로 지정해 크리티컬 효과와 추가 공격을 활용할 수 있다.",
-            "comment_en": "",
-            "comment_jp": ""
+            "comment": "염동 속성 동료로서 『환상의 인장』을 활성화하고, 조타수로 지정해 추가 공격을 활용할 수 있다.",
+            "comment_en": "As a Psy ally, activates [Illusion Seal] and can be designated as the [Helmsman] to make use of the additional attack.",
+            "comment_jp": "念動属性の仲間として『幻の印』を発動し、『操舵手』に指定して追加攻撃を活用できる。",
+            "comment_cn": "作为念动属性队员，可激活『幻之印记』，并指定为『舵手』以利用追加攻击。"
         },
         {
             "name": "아야카·여름",
-            "comment": "『여름』 동료라 『바다의 인장』을 활성화하며, 조타수로 지정해 추가 공격을 활용할 수 있다.",
-            "comment_en": "",
-            "comment_jp": ""
+            "comment": "『여름』 동료로서 『바다의 인장』을 활성화하고, 조타수로 지정해 추가 공격을 활용할 수 있다.",
+            "comment_en": "As a [Summer] ally, activates [Ocean's Seal] and can be designated as the [Helmsman] to make use of the additional attack.",
+            "comment_jp": "『夏』の仲間として『海の印』を発動し、『操舵手』に指定して追加攻撃を活用できる。",
+            "comment_cn": "作为『夏日』队员，可激活『海之印记』，并指定为『舵手』以利用追加攻击。"
         }
     ]
 };
